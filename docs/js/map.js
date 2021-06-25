aee614a182df9438931070593854cfe07469608a
AyrtonB <AyrtonBourn@Outlook.com>
2021-06-25 18:05:39 +0100

    corrected layer toggle font size

--- docs/js/map.js
+++ docs/js/map.js
@@ -122,8 +122,8 @@ document.addEventListener("DOMContentLoaded", function() {
             var mymap = L.map('map', {layers: [basemap, dateLayersGroup]}).setView([54.8, -4.61], 5);
 
             var overlayLayers = {
-                "Plants": dateLayersGroup,
-                "Network": routeLayer
+                '<span style="font-size: 0.5em">Plants</span>': dateLayersGroup,
+                '<span style="font-size: 0.5em">Network</span>': routeLayer
             };
             L.control.layers(null, overlayLayers, {position: 'bottomright'}).addTo(mymap);
 
