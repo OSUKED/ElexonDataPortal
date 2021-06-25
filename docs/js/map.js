99a1b7b441d5adb77539a84289ca6f5e9e834fbf
AyrtonB <AyrtonB@users.noreply.github.com>
2021-06-25 10:00:39 +0000

    Apply automatic changes

--- docs/js/map.js
+++ docs/js/map.js
@@ -83,7 +83,7 @@ document.addEventListener("DOMContentLoaded", function() {
         var mymap = L.map('map').setView([53.96, -3.22], 5);
 
         L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-          attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery Â© <a href="https://www.mapbox.com/">Mapbox</a>',
+          attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
           maxZoom: 18,
           id: 'mapbox/streets-v11',
           tileSize: 512,
@@ -113,6 +113,6 @@ document.addEventListener("DOMContentLoaded", function() {
             follow: 1
         });
         mymap.addControl(sliderControl);
-        sliderControl.startSlider(335);
+        sliderControl.startSlider(334);
     });
 });
