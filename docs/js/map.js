dcac149779cc408973390eaba4fb217198bbba03
AyrtonB <AyrtonB@users.noreply.github.com>
2021-07-06 06:50:13 +0000

    Apply automatic changes

--- docs/js/map.js
+++ docs/js/map.js
@@ -144,7 +144,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 follow: 1
             });
             mymap.addControl(sliderControl);
-            sliderControl.startSlider(335);
+            sliderControl.startSlider(333);
         });
     });
 });
